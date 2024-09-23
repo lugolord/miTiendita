9da33c92feb84ef86a8d04178e801e87f59e6784
Luis Gonzalez <lugo50635@gmail.com>
2024-09-23 15:43:14 -0300

    punto de partida, ultima clase

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,52 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
+
 function Checkout () {
   return (
-    <div>
-      Checkout
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h3>Resumen de compra:</h3>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <h3>Datos para la compra:</h3>
+          <Form className='border rounded p-3'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control type="text" placeholder="Enter your phone" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                Well never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="outline-success" type="submit">
+              Finalizar compra 🎉
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
